cc84b35ac3904b17a2a8c890e374e2db093ff94d
Rishabh Bhatt <52092772+rishabhbhatt@users.noreply.github.com>
2026-04-09 10:20:48 +0530

    Create src/App.jsx file with Portfolio component

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <p>Welcome to my portfolio website.</p>
+            <section>
+                <h2>Projects</h2>
+                <ul>
+                    <li>
+                        <h3>Project 1</h3>
+                        <p>Description of project 1.</p>
+                    </li>
+                    <li>
+                        <h3>Project 2</h3>
+                        <p>Description of project 2.</p>
+                    </li>
+                    <li>
+                        <h3>Project 3</h3>
+                        <p>Description of project 3.</p>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h2>About Me</h2>
+                <p>This section will contain information about me.</p>
+            </section>
+        </div>
+    );
+};
+
+export default Portfolio;
