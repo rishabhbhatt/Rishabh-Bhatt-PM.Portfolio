88100e54d3611b459cd21e8062a420af76d8d41d
Rishabh Bhatt <52092772+rishabhbhatt@users.noreply.github.com>
2026-04-09 10:20:19 +0530

    Create src/main.jsx file to import and mount the React App component

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount the React App component
+ReactDOM.render(<App />, document.getElementById('root'));
